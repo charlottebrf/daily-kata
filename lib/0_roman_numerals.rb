65407911381ceb55da78894e57a048acaf390c30
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-29 23:02:29 +0100

    Refactors the algorithm to 2 lines

--- lib/0_roman_numerals.rb
+++ lib/0_roman_numerals.rb
@@ -2,13 +2,7 @@ class RomanNumerals
 
   def convert_numeral(num)
     numeral = "I"
-    if num == 3
-      numeral * num
-    elsif num == 2
-      numeral * num
-    elsif num == 1
-      numeral * num
-    end
+    numeral * num
   end
 
 end

4027f442e1939cdbd01f84bc5b1112e66fc2c769
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-20 20:11:02 +0100

    Daily kata day 2

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,14 +1,26 @@
 class Numerals
 
-  def convert_to_numeral(arabic)
-    i = 0
-    numbers = ''
-
-    while i < 3 do
-      numbers = 'I' * arabic
-      i += 1
+  def convert(arabic)
+    one = 'I'
+    five = 'V'
+    numeral = ''
+    if arabic == 10
+      numeral = five + five
+    elsif arabic == 9
+      numeral = five + one * 4
+    elsif arabic == 8
+      numeral = five + one * 3
+    elsif arabic == 7
+      numeral = five + one * 2
+    elsif arabic == 6
+      numeral = five + one
+    elsif arabic == 5
+      numeral = five
+    elsif arabic == 4
+      numeral = one + five
+    elsif arabic == 1 || 2 || 3
+      numeral = one * arabic
     end
-    numbers
+    numeral
   end
-
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,16 +3,44 @@ require 'roman_numerals.rb'
 describe Numerals do
   subject(:numerals) { described_class.new }
 
-  it 'gives a I for a 3' do
-    expect(numerals.convert_to_numeral(1)).to eq 'I'
+  it "prints '\I' when passed a 1" do
+    expect(numerals.convert(1)).to eq ('I')
   end
-  it 'gives a II for a 2' do
-    expect(numerals.convert_to_numeral(2)).to eq 'II'
+
+  it "prints '\II' when passed a 2" do
+    expect(numerals.convert(2)).to eq ('II')
   end
-  it 'gives a III for a 3' do
-    expect(numerals.convert_to_numeral(3)).to eq 'III'
+
+  it "prints '\III' when passed a 3" do
+    expect(numerals.convert(3)).to eq ('III')
   end
-  it 'gives a IV for a 4' do
-    expect(numerals.convert_to_numeral(4)).to eq 'IV'
+
+  it "prints '\IV' when passed a 4" do
+    expect(numerals.convert(4)).to eq ('IV')
   end
+
+  it "prints '\V' when passed a 5" do
+    expect(numerals.convert(5)).to eq ('V')
+  end
+
+  it "prints '\VI' when passed a 6" do
+    expect(numerals.convert(6)).to eq ('VI')
+  end
+
+  it "prints '\VII' when passed a 7" do
+    expect(numerals.convert(7)).to eq ('VII')
+  end
+
+  it "prints '\VIII' when passed an 8" do
+    expect(numerals.convert(8)).to eq ('VIII')
+  end
+
+  it "prints '\VIIII' when passed a 9" do
+    expect(numerals.convert(9)).to eq ('VIIII')
+  end
+
+  it "prints '\VV' when passed a 10" do
+    expect(numerals.convert(10)).to eq ('VV')
+  end
+
 end

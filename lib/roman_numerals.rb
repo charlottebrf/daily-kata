362abd2217bc2a06daf94bac8071abe12461d89f
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-21 21:00:40 +0100

    Updates solution day 4

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,20 +1,17 @@
-class Numerals
+class ArabicToNumeral
   def convert(arabic)
     one = 'I'
     five = 'V'
     ten = 'X'
     numeral = ''
-    if arabic == 10
-      numeral = ten
-    elsif arabic == 9
-      numeral = one + ten
-    elsif 4 < arabic && arabic < 9
-      numeral = five + one * (arabic - 5)
-    elsif arabic == 4
-      numeral = one + five
-    else
-      numeral = one * arabic
-    end
+    numeral = ten            if arabic == 10
+    numeral = one + ten      if arabic == 9
+    numeral = five + one * 3 if arabic == 8
+    numeral = five + one * 2 if arabic == 7
+    numeral = five + one     if arabic == 6
+    numeral = five           if arabic == 5
+    numeral = one + five     if arabic == 4
+    numeral = one * arabic   if arabic <= 3
     numeral
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,45 +1,45 @@
 require 'roman_numerals.rb'
 
-describe Numerals do
-  subject(:numerals) { described_class.new }
+describe ArabicToNumeral do
+  subject(:arabic_to_numeral) { described_class.new }
 
   it "prints 'I' when passed a 1" do
-    expect(numerals.convert(1)).to eq 'I'
+    expect(arabic_to_numeral.convert(1)).to eq 'I'
   end
 
   it "prints 'II' when passed a 2" do
-    expect(numerals.convert(2)).to eq 'II'
+    expect(arabic_to_numeral.convert(2)).to eq 'II'
   end
 
   it "prints 'III' when passed a 3" do
-    expect(numerals.convert(3)).to eq 'III'
+    expect(arabic_to_numeral.convert(3)).to eq 'III'
   end
 
   it "prints 'IV' when passed a 4" do
-    expect(numerals.convert(4)).to eq 'IV'
+    expect(arabic_to_numeral.convert(4)).to eq 'IV'
   end
 
   it "prints 'V' when passed a 5" do
-    expect(numerals.convert(5)).to eq 'V'
+    expect(arabic_to_numeral.convert(5)).to eq 'V'
   end
 
   it "prints 'VI' when passed a 6" do
-    expect(numerals.convert(6)).to eq 'VI'
+    expect(arabic_to_numeral.convert(6)).to eq 'VI'
   end
 
   it "prints 'VII' when passed a 7" do
-    expect(numerals.convert(7)).to eq 'VII'
+    expect(arabic_to_numeral.convert(7)).to eq 'VII'
   end
 
-  it "prints 'VIII' when passed an 8" do
-    expect(numerals.convert(8)).to eq 'VIII'
+  it "prints 'VIII' when passed a 8" do
+    expect(arabic_to_numeral.convert(8)).to eq 'VIII'
   end
 
-  it "prints 'VIIII' when passed a 9" do
-    expect(numerals.convert(9)).to eq 'IX'
+  it "prints 'IX' when passed a 9" do
+    expect(arabic_to_numeral.convert(9)).to eq 'IX'
   end
 
   it "prints 'X' when passed a 10" do
-    expect(numerals.convert(10)).to eq 'X'
+    expect(arabic_to_numeral.convert(10)).to eq 'X'
   end
 end

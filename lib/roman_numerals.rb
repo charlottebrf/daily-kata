be6b3de421d0d6b446e91ff7874252de93cfa75f
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-22 23:23:03 +0100

    Refactors again to highlight the pattern

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,20 +2,22 @@ class Numerals
 
   def transform(arabic)
     one = 'I'
+    four = 'IV'
     five = 'V'
+    nine = 'IX'
     ten = 'X'
     numeral = ''
 
     if arabic >= 10 && arabic <= 18
       numeral = ten + transform(arabic - 10)
     elsif arabic == 9
-      numeral = one + ten
+      numeral = nine + transform(arabic - 9)
     elsif arabic >= 5 && arabic <= 8
       numeral = five + transform(arabic - 5)
     elsif arabic == 4
-      numeral = one + five
-    elsif arabic >= 0 && arabic <= 3
-      numeral = one * arabic
+      numeral = four + transform(arabic - 4)
+    elsif arabic >= 1 && arabic <= 3
+      numeral = one + transform(arabic - 1)
     end
     numeral
   end

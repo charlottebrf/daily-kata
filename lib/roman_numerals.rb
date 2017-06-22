b6611796a42ff67379e0813be8954717d4734c25
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-22 23:18:38 +0100

    Refactored numerals

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,29 +1,20 @@
 class Numerals
+
   def transform(arabic)
     one = 'I'
     five = 'V'
     ten = 'X'
     numeral = ''
 
-    if arabic >= 16 && arabic <= 18
-      numeral = (ten) + five + one * (arabic - 10)
-    elsif arabic == 15
-      numeral = ten + five
-    elsif arabic == 14
-      numeral = ten + one + five
-    elsif arabic >= 11 && arabic <= 13
-      numeral = ten + one * (arabic - 10)
-    elsif arabic == 10
-      numeral = ten
+    if arabic >= 10 && arabic <= 18
+      numeral = ten + transform(arabic - 10)
     elsif arabic == 9
       numeral = one + ten
-    elsif arabic >= 6 && arabic <= 8
-      numeral = five + one * (arabic - 5)
-    elsif arabic == 5
-      numeral = five
+    elsif arabic >= 5 && arabic <= 8
+      numeral = five + transform(arabic - 5)
     elsif arabic == 4
       numeral = one + five
-    elsif arabic >= 1 && arabic <= 3
+    elsif arabic >= 0 && arabic <= 3
       numeral = one * arabic
     end
     numeral

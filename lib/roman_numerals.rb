fe74f45abcfa242444fea0fdd5be5a66dc6501d8
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-26 22:28:52 +0100

    Refactors code

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -8,21 +8,15 @@ class RomanNumerals
     ten = 'X'
     numeral = ''
 
-    if arabic == 19
-      numeral = ten + converter(arabic - 10)
-    elsif arabic >= 15
-      numeral = ten + converter(arabic - 10)
-    elsif arabic == 14
-      numeral = ten + converter(arabic - 10)
-    elsif arabic >= 10
+    if arabic >= 10
       numeral = ten + converter(arabic - 10)
     elsif arabic == 9
       numeral = nine
-    elsif arabic >= 5 && arabic <= 8
+    elsif arabic >= 5
       numeral = five + converter(arabic - 5)
     elsif arabic == 4
       numeral = four
-    elsif arabic <= 3
+    elsif arabic >= 1
       numeral = one * arabic
     end
     numeral

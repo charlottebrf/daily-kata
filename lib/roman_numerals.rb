eda76536cee40b1780a37acd48e3679a3272ff77
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-03 09:48:14 +0100

    Completes Roman Numerals exercise to 20

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,24 +1,29 @@
-class Numerals
-
-  def transform(arabic)
+class RomanNumerals
+  def converter(number)
     one = 'I'
     four = 'IV'
     five = 'V'
-    nine = 'IX'
     ten = 'X'
-    numeral = ''
-
-    if arabic >= 10 && arabic <= 18
-      numeral = ten + transform(arabic - 10)
-    elsif arabic == 9
-      numeral = nine + transform(arabic - 9)
-    elsif arabic >= 5 && arabic <= 8
-      numeral = five + transform(arabic - 5)
-    elsif arabic == 4
-      numeral = four + transform(arabic - 4)
-    elsif arabic >= 1 && arabic <= 3
-      numeral = one + transform(arabic - 1)
+    roman = ''
+    if number >= 1 && number < 4
+      roman = one * number
+    elsif number == 4
+      roman = four
+    elsif number >= 5 && number < 9
+      roman = five + one * (number - 5)
+    elsif number == 9
+      roman = one + ten
+    elsif number >= 10 && number < 14
+      roman = ten + one * (number - 10)
+    elsif number == 14
+      roman = ten + four
+    elsif number >= 15 && number < 19
+      roman = ten + five + one * (number - 15)
+    elsif number == 19
+      roman = ten + four
+    elsif number == 20
+      roman = ten + ten 
     end
-    numeral
+    roman
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,68 +1,67 @@
-require 'roman_numerals.rb'
-describe Numerals do
-  subject(:numeral) { described_class.new }
+require 'roman_numerals'
 
-  it "gives a I when passed a 1" do
-    expect(numeral.transform(1)).to eq 'I'
-  end
-
-  it "gives a II when passed a 2" do
-    expect(numeral.transform(2)).to eq 'II'
-  end
-
-  it "gives a III when passed a 3" do
-    expect(numeral.transform(3)).to eq 'III'
-  end
-
-  it "gives a IV when passed a 4" do
-    expect(numeral.transform(4)).to eq 'IV'
-  end
-
-  it "gives a V when passed a 5" do
-    expect(numeral.transform(5)).to eq 'V'
-  end
-
-  it "gives a VI when passed a 6" do
-    expect(numeral.transform(6)).to eq 'VI'
-  end
-
-  it "gives a VII when passed a 7" do
-    expect(numeral.transform(7)).to eq 'VII'
-  end
-
-  it "gives a VIII when passed a 8" do
-    expect(numeral.transform(8)).to eq 'VIII'
-  end
-
-  it "gives a IX when passed a 9" do
-    expect(numeral.transform(9)).to eq 'IX'
-  end
-
-  it "gives a X when passed a 10" do
-    expect(numeral.transform(10)).to eq 'X'
-  end
-
-  it "gives a XI when passed a 11" do
-    expect(numeral.transform(11)).to eq 'XI'
-  end
-
-  it "gives a XII when passed a 12" do
-    expect(numeral.transform(12)).to eq 'XII'
-  end
-
-  it "gives a XIII when passed a 13" do
-    expect(numeral.transform(13)).to eq 'XIII'
-  end
-
-  it "gives a XIV when passed a 14" do
-    expect(numeral.transform(14)).to eq 'XIV'
-  end
-
-  it "gives a XV when passed a 15" do
-    expect(numeral.transform(15)).to eq 'XV'
-  end
-
-  it "gives a XVI when passed a 16" do
-    expect(numeral.transform(16)).to eq 'XVI'
+describe RomanNumerals do
+  let(:numerals) { RomanNumerals.new }
+  describe '#converter' do
+    it 'gives an I when given 1' do
+      expect(numerals.converter(1)).to eq 'I'
+    end
+    it 'gives a II when given 2' do
+      expect(numerals.converter(2)).to eq 'II'
+    end
+    it 'gives a III when given 3' do
+      expect(numerals.converter(3)).to eq 'III'
+    end
+    it 'gives a IV when given 4' do
+      expect(numerals.converter(4)).to eq 'IV'
+    end
+    it 'gives a V when given 5' do
+      expect(numerals.converter(5)).to eq 'V'
+    end
+    it 'gives a VI when given 6' do
+      expect(numerals.converter(6)).to eq 'VI'
+    end
+    it 'gives a VII when given 7' do
+      expect(numerals.converter(7)).to eq 'VII'
+    end
+    it 'gives a VIII when given 8' do
+      expect(numerals.converter(8)).to eq 'VIII'
+    end
+    it 'gives a IX when given 9' do
+      expect(numerals.converter(9)).to eq 'IX'
+    end
+    it 'gives a X when given 10' do
+      expect(numerals.converter(10)).to eq 'X'
+    end
+    it 'gives a XI when given 11' do
+      expect(numerals.converter(11)).to eq 'XI'
+    end
+    it 'gives a XII when given 12' do
+      expect(numerals.converter(12)).to eq 'XII'
+    end
+    it 'gives a XIII when given 13' do
+      expect(numerals.converter(13)).to eq 'XIII'
+    end
+    it 'gives a XIV when given 14' do
+      expect(numerals.converter(14)).to eq 'XIV'
+    end
+    it 'gives a XV when given 15' do
+      expect(numerals.converter(15)).to eq 'XV'
+    end
+    it 'gives a XVI when given 16' do
+      expect(numerals.converter(16)).to eq 'XVI'
+    end
+    it 'gives a XVII when given 17' do
+      expect(numerals.converter(17)).to eq 'XVII'
+    end
+    it 'gives a XVIII when given 18' do
+      expect(numerals.converter(18)).to eq 'XVIII'
+    end
+    it 'gives a XIV when given 19' do
+      expect(numerals.converter(19)).to eq 'XIV'
+    end
+    it 'gives a XX when given 20' do
+      expect(numerals.converter(20)).to eq 'XX'
+    end
   end
 end
